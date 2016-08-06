8dac41bd5f796f053c21c0faf491ed7ebff9d4ad
nathanjent <nathanjent@gmail.com>
2016-08-06 11:17:51 -0400

    Restructuring architecture

---
+++ src/legendsof1f600.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from flask import Flask, request, redirect
+from twilio import twilio.twiml
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+  """Respond to incoming calls with a simple text message."""
+  resp = twilio.twiml.Response()
+  resp.message("Hello, Mobile Monkey")
+  return str(resp)
+
+if __name__ == "__main__":
+  app.run(debug=True)
