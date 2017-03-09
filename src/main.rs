1cbad866cae4d472d2d1c035602551ef2cabd2fd
Nathan Jent <n.jent@edaptive.com>
2017-03-09 12:38:26 -0500

    Update to deserialize from environment variables.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ extern crate dotenv;
 extern crate envy;
 
 use dotenv::dotenv;
-use rouille::{Request};
+use rouille::Request;
 
 use std::ascii::AsciiExt;
 use std::env;
@@ -21,40 +21,42 @@ mod world;
 
 #[derive(Deserialize, Debug)]
 struct CGIRequest {
-    request_method: String,
+    #[serde(rename = "REQUEST_METHOD")]
+    method: String,
     request_uri: String,
+    #[serde(default = "Vec::new")]
     headers: Vec<(String, String)>,
     #[serde(rename = "HTTP_UPGRADE_INSECURE_REQUESTS")]
-    https: bool,
+    https: u8,
+    server_protocol: String,
     remote_addr: SocketAddr,
+}
+
+fn into_http_version(version: String) -> tiny_http::HTTPVersion {
+    let v = version.chars().filter(|c| !c.is_numeric())
+        .map(|c| c as u8).collect::<Vec<u8>>();
+    tiny_http::HTTPVersion(v[0], v[1])
 }
 
 fn main() {
     dotenv::dotenv().ok();
     match envy::from_env::<CGIRequest>() {
-       Ok(config) => println!("{:#?}", config),
+       Ok(request) => println!("{:#?}", request),
        Err(error) => panic!("{:#?}", error),
     }
-    let mut req_builder = CGIRequest {
-        request_method: "OPTIONS".into(),
+    let mut cgi_request = CGIRequest {
+        method: "OPTIONS".into(),
         request_uri: "/".into(),
         headers: Vec::new(),
-        https: false,
+        https: 0,
+        server_protocol: "HTTP/1.1".into(),
         remote_addr: "127.0.0.1:80".parse().unwrap(),
     };
 
     for (k, v) in env::vars() {
         //println!("{:?}: {:?}", k, v);
         match &*k {
-            "AUTH_TYPE" | "CONTENT_LENGTH" | "CONTENT_TYPE" | "GATEWAY_INTERFACE" | "PATH_INFO" | "PATH_TRANSLATED" | "QUERY_STRING" | "REMOTE_HOST" | "REMOTE_IDENT" | "REMOTE_USER" | "SCRIPT_NAME" | "SERVER_NAME" | "SERVER_PORT" | "SERVER_SOFTWARE" => req_builder.headers.push((k, v)),
-    "SERVER_PROTOCOL" => {
-        let version = v.chars().filter(|c| !c.is_numeric())
-            .map(|c| c as u8).collect::<Vec<u8>>();
-        tiny_http::HTTPVersion(version[0], version[1]);
-    }
-    "REQUEST_METHOD" => req_builder.request_method = v,
-    "REQUEST_URI" => req_builder.request_uri = v,
-    "REMOTE_ADDR" => req_builder.remote_addr = v.parse().unwrap(),
+            "AUTH_TYPE" | "CONTENT_LENGTH" | "CONTENT_TYPE" | "GATEWAY_INTERFACE" | "PATH_INFO" | "PATH_TRANSLATED" | "QUERY_STRING" | "REMOTE_HOST" | "REMOTE_IDENT" | "REMOTE_USER" | "SCRIPT_NAME" | "SERVER_NAME" | "SERVER_PORT" | "SERVER_SOFTWARE" => cgi_request.headers.push((k, v)),
     _ => {},
         }
     }
@@ -64,20 +66,20 @@ fn main() {
 
     //let req = tiny_http::request::new_request(
     //    false,
-    //    Method::from_str(req_builder.method),
-    //    req_builder.url,
+    //    Method::from_str(cgi_request.method),
+    //    cgi_request.url,
     //    HTTPVersion(1, 1),
-    //    req_builder.headers,
-    //    req_builder.remote_addr,
+    //    cgi_request.headers,
+    //    cgi_request.remote_addr,
 
     //    );
 
     // I know it's fake but I'm not sure how to build a request from environment variables
     let request = Request::fake_http_from(
-        req_builder.remote_addr,
-        req_builder.request_method,
-        req_builder.request_uri,
-        req_builder.headers,
+        cgi_request.remote_addr,
+        cgi_request.method,
+        cgi_request.request_uri,
+        cgi_request.headers,
         body.into(),
     );
 
@@ -119,7 +121,7 @@ fn main() {
     let mut writer = stdout.lock();
     response.raw_print(
         writer,
-        tiny_http::HTTPVersion(1, 1),
+        into_http_version(cgi_request.server_protocol),
         &response_headers[..],
         true,
         None,
